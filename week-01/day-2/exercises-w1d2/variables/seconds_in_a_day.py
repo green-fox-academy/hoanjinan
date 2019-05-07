02c75bb4fd4932c10a6e65b13c0fe60b087a372c
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-07 17:13:13 +0800

    Fill Missing answer on Variables part

--- week-01/day-2/exercises-w1d2/variables/seconds_in_a_day.py
+++ week-01/day-2/exercises-w1d2/variables/seconds_in_a_day.py
@@ -4,3 +4,9 @@ current_seconds = 42
 
 # Write a program that prints the remaining seconds (as an integer) from a
 # day if the current time is represented by the variables
+
+remain_hour = 23 - current_hours
+remain_minutes = 59 - current_minutes
+remain_seconds = 60 - current_seconds
+remain_seconds = remain_hour * 60 ** 2 + remain_minutes * 60 + remain_seconds
+print (f"{remain_seconds}")
