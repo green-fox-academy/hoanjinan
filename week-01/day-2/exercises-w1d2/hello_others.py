5b07b4f9b4ac93e368c9110a1dbf020f831d87bb
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-07 13:20:18 +0800

    Exercises on Printing Part

---
+++ week-01/day-2/exercises-w1d2/hello_others.py
@@ -0,0 +1,11 @@
+# Greet 3 of your classmates with this program, in three separate lines
+# like:
+#
+# Hello, Esther!
+# Hello, Mary!
+# Hello, Joe!
+
+firstFriend = input("Greet 1st friend\n")
+secondFriend = input("Greet 2nd friend\n")
+thirdFriend = input("Greet 2nd friend\n")
+print(f"Hello! {firstFriend}\nHello! {secondFriend}\nHello! {thirdFriend}")
