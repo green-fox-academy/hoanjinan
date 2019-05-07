6d90f965a2376f4cbd46c00b1e4fde3363bb9451
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-07 17:12:14 +0800

    Exercises for Scanner Part

---
+++ week-01/day-2/exercises-w1d2/scanner/mile_to_km_converter.py
@@ -0,0 +1,5 @@
+# Write a program that asks for an integer that is a distance in miles,
+# then it converts that value to kilometers and prints it
+mile = input("Please enter the miles to convert: ")
+kilo = float(mile) * 1.6
+print (f"{mile} miles in kilometers is {kilo}.")
