0f3a8d139f8f89f2dfaa83c9dc97489996718c99
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-10 17:47:18 +0800

    Add two matrices

---
+++ week-01/day-5/exercises-w1d5/matrix/addition.py
@@ -0,0 +1,42 @@
+A = [[1, 2],
+     [3, 4],
+     [4, 0]]
+
+B = [[5, 6],
+     [7, 8],
+     [6, 5]]
+
+def dimension_checker(mat_A, mat_B):
+    if len(mat_A) == len(mat_B) and len(mat_A[0]) == len(mat_B[0]):
+        pass
+    else:
+        print("Please check the dimension of your matrices!")
+        exit()
+
+def matrix_add(mat_A, mat_B):
+    dimension_checker(mat_A, mat_B)
+    mat_C = []
+
+    for i in range(len(mat_A)):
+        mat_C.append([0])
+        for j in range(len(mat_A[0])):
+            if j < len(mat_A[0]) - 1:
+                mat_C[i].extend([0])
+            mat_C[i][j] = mat_A[i][j] + mat_B[i][j]
+    return mat_C
+
+print(matrix_add(A, B))
+
+#Below is the traditional way of doing addition
+#without checking the dimention and auto creation of the new matrix
+
+# C = [[0, 0],
+#      [0, 0],
+#      [0, 0]]
+
+# for i in range(len(A)):
+#     for j in range(len(A[0])):
+#         C[i][j] = A[i][j] + B[i][j]
+
+# print(C)
+
