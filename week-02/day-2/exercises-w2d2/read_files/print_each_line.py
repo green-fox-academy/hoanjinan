f49c53095b96ac3a96b5b54732961f172e1c686d
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-14 16:22:20 +0800

    Print each line of a file with error handling

---
+++ week-02/day-2/exercises-w2d2/read_files/print_each_line.py
@@ -0,0 +1,11 @@
+# Write a program that opens a file called "my-file.txt", then prints
+# each line from the file.
+# If the program is unable to read the file (for example it does not exist),
+# then it should print the following error message: "Unable to read file: my-file.txt"
+
+try:
+    f = open("my-file.txt")
+    content = f.readlines()
+    print(content)
+except FileNotFoundError:
+    print("Cannot open the file!")
