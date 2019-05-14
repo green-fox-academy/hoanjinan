0119649b21ff7cc51e581a1a0b963de37c108da8
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-14 17:04:23 +0800

    copying tic_tac_toe file into a new file

---
+++ week-02/day-2/exercises-w2d2/core/copy_file.py
@@ -0,0 +1,18 @@
+# Write a function that copies the contents of a file into another
+# It should take the filenames as parameters
+# It should return a boolean that shows if the copy was successful
+
+def copy_file(old_path, new_path):
+    try:
+        f_read = open(old_path)
+        content = f_read.readlines()
+        f_write = open(new_path, "a+")
+        for i in content:
+            f_write.write(i)
+        f_read.close()
+        f_write.close()
+    except FileNotFoundError:
+        print("The original file does not exist.")
+    
+copy_file("tic_tac_toe.py", "copy-tic_tac_toe.py")
+     
