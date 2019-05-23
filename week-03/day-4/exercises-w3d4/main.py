3a7c5574271f83c7c81bfd0b9263a396e7080d72
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-23 14:20:20 +0800

    Misunderstood before. PUT method can now replace a film into a new film

--- week-03/day-4/exercises-w3d4/main.py
+++ week-03/day-4/exercises-w3d4/main.py
@@ -77,6 +77,9 @@ def api_film_put(film_id):
             count += 1
 
         if film_id in content_dic:
+            content_dic[film_id]["title"] = "Harry Potter and the Half-Blood Prince"
+            content_dic[film_id]["year"] = 2009
+            content_dic[film_id]["description"] = "Harry Potter and the Half-Blood Prince is a 2009 fantasy film directed by David Yates and distributed by Warner Bros. Pictures.[3] It is based on J. K. Rowling's 2005 novel of the same name. The film, which is the sixth instalment in the Harry Potter film series, was written by Steve Kloves and produced by David Heyman and David Barron.[4] The story follows Harry Potter's sixth year at Hogwarts as he receives a mysterious textbook, falls in love, and attempts to retrieve a memory that holds the key to Lord Voldemort's downfall."
             response = jsonify(content_dic[film_id])
             return response
         else:
@@ -87,7 +90,34 @@ def api_film_put(film_id):
         response = jsonify(error = "Invalid API_KEY")
         response.status_code = 403
         return response
-    
+
+# @app.route("/api/films/<int:film_id>", methods = ["DELETE"])
+# def api_film_delete(film_id):
+#     content_dic = {}
+#     count = 1
+#     api_key = "hello"
+#     key = request.headers["api-key"]
+
+#     if api_key == key:
+#         f = open("/Users/hoanjinan_otoko/Desktop/epam-ds-training/greenfox/hoanjinan/week-03/day-4/exercises-w3d4/films.json", mode = "r", encoding = "utf-8")
+#         content = json.load(f)
+#         f.close()
+
+#         for i in content:
+#             content_dic[count] = {"id": i["id"], "title": i["title"], "year": i["year"], "genre": i["genre"], "description": i["description"]}
+#             count += 1
+
+#         if film_id in content_dic:
+#             response = jsonify(content_dic[film_id])
+#             return response
+#         else:
+#             response = jsonify(error = f"No film found with {film_id} ID")
+#             response.status_code = 404
+#             return response
+#     else:
+#         response = jsonify(error = "Invalid API_KEY")
+#         response.status_code = 403
+#         return response
 
 if __name__ == "__main__":
     app.run()
