2b59f121426333ef051aa9c11043c6e52e20a367
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-23 13:58:19 +0800

    PUT method is written to check if the content is exist

--- week-03/day-4/exercises-w3d4/main.py
+++ week-03/day-4/exercises-w3d4/main.py
@@ -60,6 +60,33 @@ def api_film_post(film_id):
         response.status_code = 403
         return response
 
+@app.route("/api/films/<int:film_id>", methods = ["PUT"])
+def api_film_put(film_id):
+    content_dic = {}
+    count = 1
+    api_key = "hello"
+    key = request.headers["api-key"]
+
+    if api_key == key:
+        f = open("/Users/hoanjinan_otoko/Desktop/epam-ds-training/greenfox/hoanjinan/week-03/day-4/exercises-w3d4/films.json", mode = "r", encoding = "utf-8")
+        content = json.load(f)
+        f.close()
+
+        for i in content:
+            content_dic[count] = {"id": i["id"], "title": i["title"], "year": i["year"], "genre": i["genre"], "description": i["description"]}
+            count += 1
+
+        if film_id in content_dic:
+            response = jsonify(content_dic[film_id])
+            return response
+        else:
+            response = jsonify(error = f"No film found with {film_id} ID")
+            response.status_code = 404
+            return response
+    else:
+        response = jsonify(error = "Invalid API_KEY")
+        response.status_code = 403
+        return response
     
 
 if __name__ == "__main__":
