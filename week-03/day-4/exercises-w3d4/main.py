c2648f98f739b54bdec1c078685f93e88a0ff110
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-23 14:27:20 +0800

    Added DELETE method to delete a specified film as well as remove unnecessary code

--- week-03/day-4/exercises-w3d4/main.py
+++ week-03/day-4/exercises-w3d4/main.py
@@ -2,16 +2,6 @@ from flask import Flask, request, render_template, jsonify, make_response, json
 
 app = Flask(__name__)
 
-@app.route("/api")
-def api_root():
-    response = jsonify(a = 12, b = 13)
-    return response
-
-@app.route("/api", methods = ["POST"])
-def api_root_post():
-    response = jsonify(apple = 34, key = request.headers["x-api-key"])
-    return response
-
 @app.route("/api/films")
 def api_film():
     f = open("/Users/hoanjinan_otoko/Desktop/epam-ds-training/greenfox/hoanjinan/week-03/day-4/exercises-w3d4/films.json", mode = "r", encoding = "utf-8")
@@ -91,33 +81,34 @@ def api_film_put(film_id):
         response.status_code = 403
         return response
 
-# @app.route("/api/films/<int:film_id>", methods = ["DELETE"])
-# def api_film_delete(film_id):
-#     content_dic = {}
-#     count = 1
-#     api_key = "hello"
-#     key = request.headers["api-key"]
+@app.route("/api/films/<int:film_id>", methods = ["DELETE"])
+def api_film_delete(film_id):
+    content_dic = {}
+    count = 1
+    api_key = "hello"
+    key = request.headers["api-key"]
 
-#     if api_key == key:
-#         f = open("/Users/hoanjinan_otoko/Desktop/epam-ds-training/greenfox/hoanjinan/week-03/day-4/exercises-w3d4/films.json", mode = "r", encoding = "utf-8")
-#         content = json.load(f)
-#         f.close()
+    if api_key == key:
+        f = open("/Users/hoanjinan_otoko/Desktop/epam-ds-training/greenfox/hoanjinan/week-03/day-4/exercises-w3d4/films.json", mode = "r", encoding = "utf-8")
+        content = json.load(f)
+        f.close()
 
-#         for i in content:
-#             content_dic[count] = {"id": i["id"], "title": i["title"], "year": i["year"], "genre": i["genre"], "description": i["description"]}
-#             count += 1
+        for i in content:
+            content_dic[count] = {"id": i["id"], "title": i["title"], "year": i["year"], "genre": i["genre"], "description": i["description"]}
+            count += 1
 
-#         if film_id in content_dic:
-#             response = jsonify(content_dic[film_id])
-#             return response
-#         else:
-#             response = jsonify(error = f"No film found with {film_id} ID")
-#             response.status_code = 404
-#             return response
-#     else:
-#         response = jsonify(error = "Invalid API_KEY")
-#         response.status_code = 403
-#         return response
+        if film_id in content_dic:
+            content_dic.pop(film_id)
+            response = jsonify(content_dic)
+            return response
+        else:
+            response = jsonify(error = f"No film found with {film_id} ID")
+            response.status_code = 404
+            return response
+    else:
+        response = jsonify(error = "Invalid API_KEY")
+        response.status_code = 403
+        return response
 
 if __name__ == "__main__":
     app.run()
