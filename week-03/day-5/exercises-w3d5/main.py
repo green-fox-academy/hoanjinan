f187ad8432cf14c4d13d6c98a3988a05a712c26d
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-24 10:38:51 +0800

    Displaying product list using a list and Jinja template

--- week-03/day-5/exercises-w3d5/main.py
+++ week-03/day-5/exercises-w3d5/main.py
@@ -3,13 +3,25 @@ import random
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/greeting")
 def random_lang():
     greet = ["Hello", "Hola", "Bonjour", "Guten Tag", "你好", "こんにちは"]
     name = ["Zilan", "John", "Payne", "Simon", "Steven", "Levi"]
     random_greet = random.choice(greet)
     random_name = random.choice(name)
     return render_template("random_lang.html", random_greet = random_greet, name = random_name)
+
+@app.route("/products")
+def products():
+    products = [
+        ("Milk", 3.59123),
+        ("Bread", 2.96332),
+        ("Rice", 0.64111)
+    ]
+
+    return render_template("products.html", products = products)
+
+
 
 if __name__ == "__main__":
     app.run()
