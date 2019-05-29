28213f5ff0cfa40ff5efa8267623f47f3a4895f6
hoanjinan <hoanjinan_otoko@hotmail.com>
2019-05-29 22:23:56 +0800

    Configuration class for set up the connection between database and python code using psycopg2

---
+++ week-04/day-3/exercises-w4d3/Config.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+class Config():
+    def __init__(self, host = "localhost", dbname = "car_dealer", user = "hoanjinan_otoko", password = "hoanjinan_otoko"):
+        self.host = host
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+
+    def connect(self):
+        return psycopg2.connect(
+                host = self.host,
+                dbname = self.dbname,
+                user = self.user,
+                password = self.password
+                )
